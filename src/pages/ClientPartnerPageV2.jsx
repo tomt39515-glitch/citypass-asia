9ca7817a5c3e22e153bf3bdd023ffdc7f0674e4a
tomt39515-glitch <_EMAIL__GITHUB>
2026-06-07 23:04:58 +0300

    telegram auth fix

--- src/pages/ClientPartnerPageV2.jsx
+++ src/pages/ClientPartnerPageV2.jsx
@@ -456,6 +456,8 @@ table_number:
             method: "POST",
             headers: {
               "Content-Type": "application/json",
+              "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvc3d6eXV1bWN3eGptbHRjZ2VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NTMwNzUsImV4cCI6MjA5NTEyOTA3NX0.y0CUPEoH7QZl0VbBBiugoHb2qHwx7m1olXM3GDlrPCc",
+              "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
             },
             body: JSON.stringify({
               chat_id: "8052071718",
